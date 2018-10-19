0d8baadfd92780a42d1af13c04e6ee1eb9904da2
= <jlaispx@gmail.com>
2018-10-19 12:24:26 +1100

    Add accelerometer calibration

--- datalog.py
+++ datalog.py
@@ -1,19 +1,43 @@
 from microbit import *
 import os
 name = "datalog.txt"
-g = 992
+gravity = -992 #default offset gravity
+xoff =0
+yoff =0
+
+def doCalibrate():
+    global xoff, yoff, gravity
+    iters=100
+    x=0
+    y=0
+    z=0
+    for i in range(iters):
+        x += accelerometer.get_x()
+        y += accelerometer.get_y()
+        z += accelerometer.get_z()
+        sleep(100)
+        if i%10==0:
+            display.show(int(i/10))
+    xoff = round(x/iters) #calculate average offset
+    yoff = round(y/iters)
+    gravity = round(z/iters)
+    print(xoff, yoff, gravity)
+
 def doLog():
-    x = accelerometer.get_x()
-    y = accelerometer.get_y()
-    z = accelerometer.get_z()+g
+    global xoff, yoff, gravity#pass calibration offsets
+    x = accelerometer.get_x()-xoff
+    y = accelerometer.get_y()-yoff
+    z = accelerometer.get_z()-gravity
 
     coords = "{},{},{}  ".format(x, y, z)
     # print(coords)
     return coords
 
 def startLogging():
+    global xoff, yoff, gravity#pass calibration offsets
     try:
         with open(name, "w") as f:
+            f.write("{},{},{}\n".format(xoff,yoff, gravity))
             while True:
                 coords = doLog()
                 buf = "{}\n".format(coords)    
